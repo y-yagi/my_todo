7fde687f553881835722917c697933ae2a80664e
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-04-15 16:10:20 +0900

    add migration for todos table

--- migrate/001_tables.rb
+++ migrate/001_tables.rb
@@ -1,7 +1,9 @@
 Sequel.migration do
   change do
-    create_table(:model1s) do
+    create_table(:todos) do
       primary_key :id
+      String :content, null: false
+      DateTime :deadline
     end
   end
 end
