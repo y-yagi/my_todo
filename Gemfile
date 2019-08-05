07f7549c81a969a1e69bc14c8a98182dcd3d61fd
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-08-06 08:37:58 +0900

    Add basic test

--- Gemfile
+++ Gemfile
@@ -11,5 +11,6 @@ gem 'rack-unreloader'
 gem 'puma'
 
 gem 'capybara'
+gem 'apparition'
 gem 'minitest', '>= 5.7.0'
 gem 'minitest-hooks', '>= 1.1.0'
--- spec/web/spec_helper.rb
+++ spec/web/spec_helper.rb
@@ -4,11 +4,13 @@ raise "test database doesn't end with test" unless DB.opts[:database] =~ /test\z
 
 require 'capybara'
 require 'capybara/dsl'
+require 'capybara/apparition'
 require 'rack/test'
 
 require_relative '../minitest_helper'
 
 Capybara.app = MyTodo.freeze.app
+Capybara.default_driver = :apparition
 
 class Minitest::HooksSpec
   include Rack::Test::Methods
